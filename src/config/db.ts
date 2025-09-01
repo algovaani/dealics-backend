db53af463734c4c2859234492b2a36aea7132560
unknown <er.kaurbeant@gmail.com>
2025-09-01 12:38:49 +0530

    changes in db file

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,8 @@ import { Sequelize } from "sequelize-typescript";
 export const sequelize = new Sequelize({
   database: "stagingtradeblock",
   username: "root",
-  password: "",
-  host: "localhost",
+  password: "RockyLinux@DB1",
+  host: "5.223.61.90",
   dialect: "mysql",
   logging: false,
   models: [], // Start with empty models array

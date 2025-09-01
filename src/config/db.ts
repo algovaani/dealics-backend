d55368692ad9b0d556ceb67d1c4caefc968a8bfd
unknown <er.kaurbeant@gmail.com>
2025-09-01 13:43:16 +0530

    changes in db file

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
+import mysql from "mysql2";   // ✅ add this
 
 export const sequelize = new Sequelize({
   database: "stagingtradeblock",
@@ -7,7 +8,7 @@ export const sequelize = new Sequelize({
   host: "localhost",
   port: 3306,
   dialect: "mysql",
-  dialectModule: require("mysql2"),
+  dialectModule: mysql,   // ✅ use imported mysql
   logging: false,
 });
 
